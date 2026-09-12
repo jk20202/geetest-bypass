5718a155ed79f237a6c04023bde1ee2d3eaffc38
wulu007 <130914655+wulu007@users.noreply.github.com>
2026-09-12 00:48:39 +0000

    chore: update geetest config to v1.9.6-1fe704 [auto]

--- src/wulu_geetest_bypass/config.py
+++ src/wulu_geetest_bypass/config.py
@@ -26,7 +26,7 @@ class Config:
     }
 
     biht = '1426265548'
-    lib_key = 'uONi'
-    lib_val = '4vVu'
-    abo_key = '(n[19:21]+n[19:21])+.+(n[23:26])+.+(n[18:23])'
-    abo_val = 'n[6:9]'
+    lib_key = 'fX1g'
+    lib_val = 'Uq0E'
+    abo_key = '(n[14:19])+.+(n[3:3]+n[8:8]+n[12:12]+n[0:0])+.+(n[25:28])'
+    abo_val = 'n[13:16]'
